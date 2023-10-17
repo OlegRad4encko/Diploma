6aaed05552386812f35fd777e5d83e0a57832ffd
Oleg Radchencko <57410595+OlegRad4encko@users.noreply.github.com>
2023-10-18 00:18:42 +0300

    Deutsch lang was added

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from voiceModel import VoiceModel as vm
 
 vm_ua = vm('ua', 'v4_ua', 48000, 'mykyta')
 vm_en = vm('en', 'v3_en', 48000, 'en_0')
+vm_de = vm('de', 'v3_de', 48000, 'karlsson')
 
 text_ua = 'Привіт, мене звати Олег'
 vm_ua.play_audio(text_ua)
 
 text_en = 'Hello, my name is Oleg'
 vm_en.play_audio(text_en)
+
+text_de = 'Ich glaube, ich höre Oleg'
+vm_de.play_audio(text_de)
