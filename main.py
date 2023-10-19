b53ad88704f8e4e4ce113291d5e9b2155723442a
Oleg Radchencko <57410595+OlegRad4encko@users.noreply.github.com>
2023-10-19 18:48:43 +0300

    Fix TRA-1 issue & Test it with in a bundle voiveModel module

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import json
 with open('languages.json', 'r') as vm_config_file:
     languages = json.load(vm_config_file)
 
+
+vm_de = vm(languages['German']['language'], languages['German']['model_id'], languages['German']['sample_rate'],
+           languages['German']['speaker'])
 vm_en = vm(languages['English']['language'], languages['English']['model_id'], languages['English']['sample_rate'],
            languages['English']['speaker'])
-translate = tr('UK', 'EN')
-vm_en.play_audio(translate.translate('Добрий ранок'))
 
-# vm_ua = vm(languages['Ukraine']['language'], languages['Ukraine']['model_id'], languages['Ukraine']['sample_rate'],
-#           languages['Ukraine']['speaker'])
+text = 'Привіт, мене звати Олег. Приємно познайомитись'
+
+trans = tr('UK', 'EN')
+vm_en.play_audio(trans.translate_text(text))
+
+trans.change_langs('DE')
+vm_de.play_audio(trans.translate_text(text))
+
 
 
 
--- translator.py
+++ translator.py
@@ -2,15 +2,18 @@ from translate import Translator
 
 
 class TranslatorModel:
-    def __init__(self, to_lang: str, from_lang: str):
-        self.translator = Translator(from_lang, to_lang)
+    def __init__(self, from_lang: str, to_lang: str):
+        self.to_lang = to_lang
+        self.from_lang = from_lang
 
     def change_langs(self, to_lang: str, from_lang: str = None):
+        self.to_lang = to_lang
         if from_lang is not None:
-            self.translator = Translator(from_lang, to_lang)
-        else:
-            self.translator = Translator(to_lang)
+            self.from_lang = from_lang
 
-    def translate(self, text_to_translate: str) -> str:
-        return self.translator.translate(text_to_translate)
+    def translate_text(self, text_to_translate: str) -> str:
+        translator = Translator(from_lang=self.from_lang, to_lang=self.to_lang)
+        return translator.translate(text_to_translate)
 
+
+    # разобраться с изменением языка
