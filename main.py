c2ef539ed346b8822769c39ac68cde54bfade2a1
Oleg Radchencko <57410595+OlegRad4encko@users.noreply.github.com>
2023-11-13 03:37:07 +0200

    testSettings

--- main.py
+++ main.py
@@ -10,92 +10,81 @@ import asyncio
 import config_assistant as config
 
 
-# Configs #
+from flask import Flask, send_file, render_template, url_for
+import webbrowser
+
+app = Flask(__name__, template_folder='web')
+
+@app.route('/')
+def serve_html():
+    return render_template('index.html')
+
+@app.route('/other_settings')
+def other_settings():
+    return render_template('otherSettings.html')
+
+@app.route('/src/styles.css')
+def serve_css():
+    return send_file('src/styles.css', mimetype='text/css')
 
 
-with open('conf_files/languages_tts.json', 'r') as vm_config_file:
-    languages = json.load(vm_config_file)
 
-with open('conf_files/freeGPT.json', 'r') as gpt_config_file:
-    gpt_configs = json.load(gpt_config_file)
+def open_browser():
+    webbrowser.open('http://127.0.0.1:5000/')
 
-
-# End of configs #
-
-
-
-
-def execute_cmd(cmd: str):
-    print("Виконую: "+cmd)
-
-def recognize_cmd(cmd: str):
-    rc = {'cmd': '', 'percent': 0}
-    for c, v in config.ASSISTANT_CMD_LIST.items():
-
-        for x in v:
-            vrt = fuzz.ratio(cmd, x)
-            if vrt > rc['percent']:
-                rc['cmd'] = c
-                rc['percent'] = vrt
-
-    return rc
-
-def stt_respond(voice: str):
-    print(voice)
-    if voice.startswith(config.ASSISTANT_ALIAS):
-        cmd = recognize_cmd(filter_cmd(voice))
-
-        if cmd['cmd'] not in config.ASSISTANT_CMD_LIST.keys():
-            print("Я вас не зрозумів")
-        else:
-            execute_cmd(cmd['cmd'])
-
-
-def filter_cmd(raw_voice: str):
-    cmd = raw_voice
-
-    for x in config.ASSISTANT_ALIAS:
-        cmd = cmd.replace(x, "").strip()
-
-    return cmd
+if __name__ == '__main__':
+    import threading
+    server_thread = threading.Thread(target=app.run, kwargs={'host': '127.0.0.1', 'port': 5000})
+    server_thread.start()
+    open_browser()
+    server_thread.join()
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# def execute_cmd(cmd: str):
+#     print("Виконую: "+cmd)
+#
+# def recognize_cmd(cmd: str):
+#     rc = {'cmd': '', 'percent': 0}
+#     for c, v in config.ASSISTANT_CMD_LIST.items():
+#         for x in v:
+#             vrt = fuzz.ratio(cmd, x)
+#             if vrt > rc['percent']:
+#                rc['cmd'] = c
+#                rc['percent'] = vrt
+# #
+#     return rc
+# #
+# def stt_respond(voice: str):
+#     print(voice)
+#     if voice.startswith(config.ASSISTANT_ALIAS):
+#         cmd = recognize_cmd(filter_cmd(voice))
+#
+#         if cmd['cmd'] not in config.ASSISTANT_CMD_LIST.keys():
+#             print("Я вас не зрозумів")
+#         else:
+#             execute_cmd(cmd['cmd'])
+#
+#
+# def filter_cmd(raw_voice: str):
+#     cmd = raw_voice
+#
+#     for x in config.ASSISTANT_ALIAS:
+#         cmd = cmd.replace(x, "").strip()
+#
+#     return cmd
+#
 # stt = stt('vosk-model-uk-v3')
 # listen(stt_respond, stt.get_model())
 
 
 
-async def gpt_req(text_to_gpt):
-    #################
-    freeGPT = gpt()
-    #################
-    return await freeGPT.create_prompt(text_to_gpt)
-
-
-if __name__ == "__main__":
-     result = asyncio.run(gpt_req("Привет, как твои дела?"))
-print(result)
-
-
-
-
-
-
-
+# async def gpt_req(text_to_gpt):
+#     freeGPT = gpt()
+#     return await freeGPT.create_prompt(text_to_gpt)
+#
+# if __name__ == "__main__":
+#     print(asyncio.run(gpt_req("Что такое Дота?")))
